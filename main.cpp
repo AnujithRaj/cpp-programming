bcfdc9cbe9faca5882c0d2efe495eff11c544471
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-09 16:31:44 +0530

    Add basic C++ hello world program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  // Libraries
 
-int main() {
-    cout << "Hello, World!";
-    return 0;
+using namespace std;    // namespace
+
+int main() {    // Main function
+
+    cout << "Hello, World!" << endl;
+    cout << "Hi! I am Anujith" << endl;   // All the code
+    cout << "I am learning C++" << endl;
+
+    return 0;   // Return to operating system
 }
