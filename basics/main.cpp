b6e76868f69fd3e3945ed3819b01c771e61cfe3b
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-14 18:07:11 +0530

    Update main.cpp files

---
+++ basics/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>  // Libraries
+
+using namespace std;    // namespace
+
+int main() {    // Main function
+
+    cout << "Hello, World!" << endl;
+    cout << "Hi! I am Anujith" << endl;   // All the code
+    cout << "I am learning C++" << endl;
+
+    // Integer value
+    int x = 0;
+    x = 10;
+    cout << x << endl;
+
+    // Input value 
+    int age;
+    cin >> age;
+    cout << "Hi, So your age is... " << age << endl;
+
+    char initial;
+    cin >> initial;
+    cout << "Hi! So the initial letter of your name is " << initial << endl;
+
+    int a;
+    float b;
+
+    cin >> a >> b;
+    
+    cout << "value of a is " << a << endl;
+    cout << "value of b is " << b << endl;
+
+    
+
+    return 0;   // Return to operating system
+}
