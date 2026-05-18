3eacff8a42f3a68273ec0535c2bbf76f30c817b4
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-18 17:50:43 +0530

    Updated C++ basics programs

---
+++ basics/Variables_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Take name as input and print.
+    // Take two numbers and print their: sum, difference, product, and division.
+    // Take age input and print.
+    // Take length and breadth of rectangle and print area:
+    // Convert temperature from Celsius to Fahrenheit.
+    
+}
---
+++ basics/if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    {
+        int year = 2020;
+
+        if (year > 2000){
+            cout << "Welcom to teh 21th century!" << endl;
+            // true statement
+        }
+        else {
+            cout << "HOpe to see you soon in the future!" << endl;
+        }
+
+        // If its > 2000, then "Welcome to the 21st cintury"
+        // anything else, then "HOpe to see you soon in the furure!"
+    }
+
+    return 0;
+}
